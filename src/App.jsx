44f1db6c81a9a73274cf5199cfdeac2f8a49ba9a
rzzza_rehan <rhn.raza121@gmail.com>
2024-08-30 18:19:54 +0530

    Printing logs

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
       <GameBoard onSelectSquare={ handleSelectSquare }
         turns={ gameTurns }/>
     </div>
-    <Log />
+    <Log turns={gameTurns} />
   </main>
 }
 
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,5 +1,11 @@
-export default function Log({player, symbol, row, col}) {
+export default function Log({turns}) {
     return <>
-        <p>{player} has put {symbol} on {row}-{col}</p>
+        <ol id="log">
+            {turns.map((turn) => (
+                <li key={`${turn.square.row}${turn.square.col}`}>
+                    {turn.player} selected row {turn.square.row}, col {turn.square.col}
+                </li>
+            ))}
+        </ol>
     </>
 }
