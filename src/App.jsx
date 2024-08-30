3be13538b5eb6bc6cb9b11a33a753521cb24ac5d
rzzza_rehan <rhn.raza121@gmail.com>
2024-08-30 18:48:50 +0530

    removed activePlayer state

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,27 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/Gameboard";
 import Log from "./components/Log";
+
+function derivedActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if(gameTurns.length > 0 && gameTurns[0].player=="X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [ gameTurns, setGameTurns ] = useState([]);
-  const [ activePlayer, setActivePlayer ] = useState('X');
+  // const [ activePlayer, setActivePlayer ] = useState('X');
+  const activePlayer = derivedActivePlayer(gameTurns);
   
   function handleSelectSquare(row, col) {
     // this state for Player component
-    setActivePlayer((currActivePlayer)=> currActivePlayer === 'X' ? 'O':'X');
+    // setActivePlayer((currActivePlayer)=> currActivePlayer === 'X' ? 'O':'X');
     
     // this state for GameBoard component
     setGameTurns((prevTurn) => {
-      let currentPlayer = "X";
-      
-      if(prevTurn.length > 0 && prevTurn[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = derivedActivePlayer(prevTurn);
 
       const updatedTurns = [{square: {row, col}, player: currentPlayer}, ...prevTurn];
       
